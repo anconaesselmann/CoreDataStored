ae2388400ceabaf4a3418e9a5cc40a0c60065ced
Axel Ancona Esselmann <axel@anconaesselmann.com>
2024-03-07 09:25:37 -0800

    minEncodedOptional int

--- Sources/CoreDataStored/CoreData+Extensions/OptionalInt/Int+minEncodedOptional.swift
+++ Sources/CoreDataStored/CoreData+Extensions/OptionalInt/Int+minEncodedOptional.swift
@@ -1,8 +1,33 @@
-//
-//  File.swift
-//  
-//
 //  Created by Axel Ancona Esselmann on 3/6/24.
 //
 
 import Foundation
+
+public extension Int {
+
+    enum Error: Swift.Error {
+        case underflow
+        case overflow
+    }
+
+    init(minEncodedOptional value: Int?) throws {
+        guard let value = value else {
+            self = .min
+            return
+        }
+        guard value > Int(Self.min) else {
+            throw Error.underflow
+        }
+        guard value <= Int(Self.max) else {
+            throw Error.overflow
+        }
+        self = Int(value)
+    }
+
+    var minEncodedOptional: Int? {
+        guard self != Self.min else {
+            return nil
+        }
+        return Int(self)
+    }
+}
